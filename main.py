8ebe503b14d7623d526757c76d84a58f713f9635
prandot <thiago.prando+linux@gmail.com>
2022-01-26 22:57:09 +0100

    first commit 2

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas
+
+# TODO 1. Create a dictionary in this format:
+ita_dict = {row.LETTERA: row.PAROLA for (index, row) in pandas.read_csv("spelling_italiano.csv").iterrows()}
+# print(nato_dict)
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+tospell = input("Scrivi una parola da fare lo spelling: ").upper().replace(" ", "")
+result = [ita_dict[n] for n in tospell]
+print(result)
